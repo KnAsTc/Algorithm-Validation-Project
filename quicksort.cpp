ac083805371320b99401b7bc3e206c0c20b01095
KnAsTc <59218484+KnAsTc@users.noreply.github.com>
2024-07-15 17:00:08 +0800

    Create quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,62 @@
+/*
+index: i = front - 1
+	   j = front ~ end - 1 
+*/ 
+/*
+C qsort
+int cmp(const void *a, const void *b)
+{
+return *(int *)a - *(int *)b;
+}
+qsort(array, size of array, sizeof(int), cmp);
+*/
+#include <iostream>
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+} 
+
+int Partition(int *arr, int front, int end){
+    int pivot = arr[end];
+    int i = front -1;
+    for (int j = front; j < end; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    i++;
+    swap(&arr[i], &arr[end]);
+    return i;
+}
+
+
+void QuickSort(int *arr, int front, int end){
+    if (front < end) {
+        int pivot = Partition(arr, front, end);
+        QuickSort(arr, front, pivot - 1);
+        QuickSort(arr, pivot + 1, end);
+    }
+}
+
+void PrintArray(int *arr, int size){
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+int main() {
+
+    int n = 9;
+    int arr[] = {9, 4, 1, 6, 7, 3, 8, 2, 5};
+    std::cout << "original:\n";
+    PrintArray(arr, n);
+
+    QuickSort(arr, 0, n-1);
+
+    std::cout << "sorted:\n";
+    PrintArray(arr, n);
+    return 0;
+}
